6f5a068d0924ceb9b40714502dd14c7c83b44358
Gmmaina <mutondogm@gmail.com>
2024-07-24 12:25:04 +0300

    first app

---
+++ DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.diceroller.databinding.ActivityMainBinding
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding.rollBtn.setOnClickListener{
+            rollDice()
+        }
+
+    }
+
+    private fun rollDice() {
+        val randomInt = Random.nextInt(1,7)
+        val diceImage = when(randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        binding.diceImageView.setImageResource(diceImage)
+        Toast.makeText(this, "Random number is $randomInt", Toast.LENGTH_SHORT).show()
+    }
+}
